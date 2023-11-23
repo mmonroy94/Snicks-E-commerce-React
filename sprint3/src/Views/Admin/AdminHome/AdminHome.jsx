565bef396aafa7a0ecb0a2a7454066dcb25571dc
Melissa Monroy <melissamonroy@hotmail.com>
2023-11-23 15:43:39 -0500

    amdin actualizaciones 2

--- sprint3/src/Views/Admin/AdminHome/AdminHome.jsx
+++ sprint3/src/Views/Admin/AdminHome/AdminHome.jsx
@@ -1,13 +1,32 @@
-import style from './AdminHome.module.css'
-import { useDispatch } from 'react-redux';
-import { useState } from 'react';
-import axios from 'axios';
-import { validateProduct } from '../../../formValidation';
 import { useSelector } from 'react-redux'; 
+import { useState } from 'react';
+import { Modal } from "antd";
 
 const AdminHome = () => {
-    const dispatch = useDispatch()
     const products = useSelector(state => state.allProducts)    
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const showModal = () => {
+      setModalVisible(true);
+    };
+  
+    const modalHandleClose = () => {
+      setModalVisible(false);
+    };
+  
+    const [selectedProduct, setSelectedProduct] = useState();
+    console.log(selectedProduct);
+
+    const [modifiedProduct, setModifiedProduct] = useState({
+            name: '',
+            brand: '',
+            price: '',
+            stock: '',
+            detail: '',
+            image: '',
+            gender: []
+        }
+    );
 
     // const genders = ['Mujer','Hombre', 'Unisex']
     // const [data, setData] = useState({
@@ -104,6 +123,7 @@ const AdminHome = () => {
                 <tbody>
                   
                 {products?.map((product, index) => {
+
                 return (
                     <tr>
                     <th scope="row">{index}</th>
@@ -113,16 +133,78 @@ const AdminHome = () => {
                     <td>{product.brand}</td>
                     <td>{product.gender}</td>
                     <td>{product.price}</td>
-                    <td>PDTE</td>
-                    <td><button className="px-3 py-1 mx-2 btn btn-outline-primary" type="button"><i class="bi bi-pencil"></i></button></td>
-                    <td><button className="px-3 py-1 mx-2 btn btn-outline-primary">Ver detalle</button></td>  
+                    <td>{product.quantity}</td>
+                    <td><button className="px-3 py-1 mx-2 btn btn-outline-primary" type="button" onClick={() => {showModal(); setSelectedProduct(product)}}><i class="bi bi-pencil"></i></button></td>
                     </tr>
                 )
             })}
                 </tbody>
                 </table>
-              </div>  
-              
+                {selectedProduct&&
+                <Modal
+                  title='Modificar producto'
+                  open={modalVisible}
+                  onOk={modalHandleClose}
+                  onCancel={modalHandleClose}
+                  >
+                  <form class="was-validated">
+                    <div class="mb-3">
+                      <label for="name" class="form-label">Nombre producto</label>
+                      <input type="text" class="form-control" id="name" value={selectedProduct.name} required/>
+                      <div class="invalid-feedback">
+                        Ingresa un nombre de producto de minimo 3 y maximo 20 caracteres
+                      </div>
+                    </div>
+
+                    <div class="mb-2">
+                      <label for="name" class="form-label">Nombre producto</label>
+                      <input type="text" class="form-control" id="name" value={selectedProduct.name}/>
+                    </div>
+                    <div class="mb-2">
+                      <label for="type" class="form-label">Tipo (Estilo / Funcionalidad)</label>
+                      <input type="text" class="form-control" id="type" value={selectedProduct.type}/>
+                    </div>
+                    <div class="mb-2">
+                      <label for="brand" class="form-label">Marca</label>
+                      <input type="text" class="form-control" id="brand" value={selectedProduct.brand}/>
+                    </div>
+                    <div class="mb-2">
+                      <label for="gender" class="form-label">Género</label>
+                      <input type="text" class="form-control" id="gender" value={selectedProduct.gender}/>
+                      {/* <select class="form-select" aria-label="Default select example">
+                        <option selected>Selecciona una opción</option>
+                        <option value="Hombre">Hombre</option>
+                        <option value="Mujer">Mujer</option>
+                        <option value="Unisex">Unisex</option>
+                      </select>     */}
+                    </div>
+                    <div class="mb-2">
+                      <label for="price" class="form-label">Precio - USD</label>
+                      <input type="text" class="form-control" id="price" value={selectedProduct.price}/>
+                    </div>
+                    <div class="mb-2">
+                      <label for="quantity" class="form-label">Cantidad / Stock</label>
+                      <input type="text" class="form-control" id="quantity" value={selectedProduct.quantity}/>
+                    </div>
+                    <div class="mb-2">
+                    <label htmlFor="productDetail" className="form-label">Descripción del producto</label>
+                    <textarea type="text" name='detail' value={selectedProduct.description} class="form-control" id="productDetail" rows="3"></textarea>
+                    </div>
+                    <div class="mb-2">
+                      <label for="image" class="form-label">URL imagen</label>
+                      <input type="text" class="form-control" id="image" value={selectedProduct.image}/>
+                    </div>
+                    {selectedProduct.image && (
+                    <div class="mb-2">
+                      <p>Vista imagen actual</p>
+                      <img src={selectedProduct.image} alt={selectedProduct.name} className='img-fluid'/>
+                    </div>
+                    )}
+
+                  </form>
+                  </Modal>}
+
+              </div>   
 
     )   
 }
